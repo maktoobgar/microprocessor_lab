ccc42cec6f88b2cbe83e8dd497a7a6c327d8e6a9
Mahmood Heidari <mahmoodh1378@gmail.com>
2021-12-26 21:44:42 +0330

    ✅ added customized speed to rotations

--- Final/main.cpp
+++ Final/main.cpp
@@ -5,16 +5,19 @@
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 
-const float delays[] = {0.1f, 0.09f, 0.08f, 0.07f, 0.06f, 0.05f, 0.04f, 0.03f, 0.02f, 0.01f};
+const float delays[] = {0.05f, 0.1f};
 // ((F_CPU / 256) * 0.1)
 // 0.1 = 100ms wait for stepper to make the move
-float COUNT = (F_CPU / float(256)) * delays[0];
+float COUNT[] = {(F_CPU / float(256)) * delays[0], (F_CPU / float(256)) * delays[1]};
 
-int counter = 0;
+int counter_right = 0;
+int counter_left = 0;
 int step_right = 0;
 int step_left = 0;
 bool right = false;
 bool left = false;
+int right_speed = 0;
+int left_speed = 0;
 
 unsigned char steps[] = {0b0101, 0b0110, 0b1010, 0b1001};
 
@@ -30,32 +33,37 @@ void _timer0_initialization()
     TCNT0 = 0x00;
 }
 
-void next_move()
+void next_right_move()
 {
-    if (right)
+    if (++step_right >= 4)
     {
-        if (++step_right >= 4)
-        {
-            step_right = 0;
-        }
-        PORTA = steps[step_right];
+        step_right = 0;
     }
-    if (left)
+    PORTA = steps[step_right];
+}
+
+void next_left_move()
+{
+    if (++step_left >= 4)
     {
-        if (++step_left >= 4)
-        {
-            step_left = 0;
-        }
-        PORTB = steps[step_left];
+        step_left = 0;
     }
+    PORTB = steps[step_left];
 }
 
 ISR(TIMER0_OVF_vect)
 {
-    if (++counter >= COUNT)
+    if (++counter_right >= COUNT[right_speed])
     {
-        counter = 0;
-        next_move();
+        counter_right = 0;
+        if (right)
+            next_right_move();
+    }
+    if (++counter_left >= COUNT[left_speed])
+    {
+        counter_left = 0;
+        if (left)
+            next_left_move();
     }
 }
 
@@ -74,22 +82,30 @@ int main()
         if      ((comp == 0b00000011) || (comp == 0b00000001)) {
             right=false;
             left=true;
+            left_speed = 1;
         }
         else if ((comp == 0b00000110) || (comp == 0b00000010)) {
-            right=false;
+            right=true;
             left=true;
+            right_speed = 1;
+            left_speed = 0;
         }
         else if (comp == 0b00000100) {
             right=true;
             left=true;
+            right_speed = 0;
+            left_speed = 0;
         }
         else if ((comp == 0b00001100) || (comp == 0b00001000)) {
             right=true;
-            left=false;
+            left=true;
+            right_speed = 0;
+            left_speed = 1;
         }
         else if ((comp == 0b00011000) || (comp == 0b00010000)) {
             right=true;
             left=false;
+            right_speed = 1;
         }
         else {
             right=false;
